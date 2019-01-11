4751e56fdf0ebf5da3e2510a1b6e532a7d4f3186
DontSpeaking <18306874483@sina.cn>
2019-01-11 18:16:41 +0800

    init

--- usercenter-provider/src/main/java/com/hypersmart/usercenter/mapper/SysLogsMapper.java
+++
@@ -1,7 +0,0 @@
-package com.hypersmart.usercenter.mapper;
-
-import com.hypersmart.framework.mapper.GenericMapper;
-import com.hypersmart.usercenter.model.SysLogs;
-
-public interface SysLogsMapper extends GenericMapper<SysLogs> {
-}
--- usercenter-provider/src/main/java/com/hypersmart/usercenter/service/SysLogsService.java
+++
@@ -1,7 +0,0 @@
-package com.hypersmart.usercenter.service;
-
-import com.hypersmart.usercenter.model.SysLogs;
-import com.hypersmart.framework.service.IGenericService;
-
-public interface SysLogsService extends IGenericService<String, SysLogs> {
-}
