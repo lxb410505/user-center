c3966035f2030f3e0e493354579e2e161bc1e2cc
DontSpeaking <18306874483@sina.cn>
2019-02-15 10:12:18 +0800

    添加组织列表查询接口

--- usercenter-provider/src/main/java/com/hypersmart/usercenter/controller/UcOrgController.java
+++ usercenter-provider/src/main/java/com/hypersmart/usercenter/controller/UcOrgController.java
@@ -40,7 +40,7 @@ public class UcOrgController extends BaseController {
 
     @PostMapping({"/queryList"})
     @ApiOperation(value = "组织架构数据列表}", httpMethod = "POST", notes = "获取组织架构列表")
-    public PageList<UcOrg> list(@ApiParam(name = "queryFilter", value = "查询对象") @RequestBody QueryFilter queryFilter) {
+    public PageList<UcOrg> queryList(@ApiParam(name = "queryFilter", value = "查询对象") @RequestBody QueryFilter queryFilter) {
         return this.ucOrgService.query(queryFilter);
     }
 
