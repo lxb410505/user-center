c500608b7f3ec526fbf4d0dfb94cd4c8dd3900b7
lily <ph123456>
2019-04-11 19:11:55 +0800

    增加接口：queryChildrenByCondition

--- usercenter-provider/src/main/java/com/hypersmart/usercenter/service/impl/UcOrgServiceImpl.java
+++ usercenter-provider/src/main/java/com/hypersmart/usercenter/service/impl/UcOrgServiceImpl.java
@@ -516,20 +516,14 @@ public class UcOrgServiceImpl extends GenericService<String, UcOrg> implements U
         }
 
         Set<String> list=new HashSet<String>();
-        List<UcOrgUser> list1 =ucOrgUserService.getUserDefaultOrg(userId);
-        //查询用户所在非默认维度组织的引用默认组织（查询用户所在条线对应的默认组织，只查询地块级别）
-        List<UcOrgUser> list2 =ucOrgUserService.getUserDefaultOrgByRef(userId);
-
+        List<UcOrg> list1 =getUserOrgListMerge(userId);
 
         if(list1!=null && list1.size()>0){
-           for(UcOrgUser o1 : list1){
-               list.add(o1.getOrgId());
+           for(UcOrg o : list1){
+               if("1".equals(o.getDisabled())){
+                   list.add(o.getId());
+               }
            }
-        }
-        if(list2!=null && list2.size()>0){
-            for(UcOrgUser o2 : list2){
-                list.add(o2.getOrgId());
-            }
         }
 
         if (null == list || list.size() <= 0) {
