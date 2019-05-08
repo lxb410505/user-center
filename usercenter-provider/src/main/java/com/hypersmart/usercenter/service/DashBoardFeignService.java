5c3d9963b3ed38f02ed7f98472327968db175498
lijia <lI19940621>
2019-05-08 09:38:07 +0800

    修改

--- usercenter-provider/src/main/java/com/hypersmart/usercenter/service/DashBoardFeignService.java
+++ usercenter-provider/src/main/java/com/hypersmart/usercenter/service/DashBoardFeignService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
 
-@FeignClient(name = "dashboard-provider-eureka-lj", fallback = DashBoardFeignService.DashBoardFeignServiceImpl.class, configuration = {FeignConfig.class})
+@FeignClient(name = "dashboard-provider-eureka", fallback = DashBoardFeignService.DashBoardFeignServiceImpl.class, configuration = {FeignConfig.class})
 public interface DashBoardFeignService {
     @RequestMapping(value = "/dashboard/api/gridBasicInfo/handChangeRange", method = RequestMethod.POST)
     public void handChangeRange(@RequestParam("gridId")String gridId,@RequestParam("gridRange")String gridRange,@RequestParam("action")Integer action);
