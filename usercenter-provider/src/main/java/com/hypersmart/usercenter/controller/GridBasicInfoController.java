8b79d4db795043e42979b6ae7186548b46acf6fc
yong <3>
2019-05-11 01:07:41 +0800

    修改mapper文件

--- usercenter-provider/src/main/java/com/hypersmart/usercenter/controller/GridBasicInfoController.java
+++ usercenter-provider/src/main/java/com/hypersmart/usercenter/controller/GridBasicInfoController.java
@@ -298,7 +298,8 @@ public class GridBasicInfoController extends BaseController {
 	@GetMapping({"/getHouseByCondition"})
 	@ApiOperation(value = "根据地块id，获取地块下的网格覆盖的房产信息", httpMethod = "GET", notes = "根据地块id，获取地块下的楼栋网格信息")
 	public List<Map<String,Object>> getHouseByCondition(@ApiParam(name = "id", value = "地块id", required = true) @RequestParam("divide") String divide,@RequestParam(value = "id",required = false,defaultValue = "0") String id) {
-		List<Map<String, Object>> houseByCondition = gridBasicInfoService.getHouseByCondition(divide, id);
+		return gridBasicInfoService.getHouseByCondition(divide, id);
+		/*List<Map<String, Object>> houseByCondition = gridBasicInfoService.getHouseByCondition(divide, id);
 
 		Map<String,Object> checkMap = new HashMap<>(16);
 		List<Map<String, Object>> maps = new ArrayList<>();
@@ -312,6 +313,6 @@ public class GridBasicInfoController extends BaseController {
 			}
 
 		}
-		return maps;
+		return maps;*/
 	}
 }
