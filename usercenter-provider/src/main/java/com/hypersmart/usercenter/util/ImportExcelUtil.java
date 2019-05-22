8501f3b5ffcd1ecd81b3857f7ae0ddc667bcb645
liuhao <121935403@qq.com>
2019-05-22 18:36:34 +0800

    客户满意度。

--- usercenter-provider/src/main/java/com/hypersmart/usercenter/util/ImportExcelUtil.java
+++ usercenter-provider/src/main/java/com/hypersmart/usercenter/util/ImportExcelUtil.java
@@ -153,7 +153,49 @@ public class ImportExcelUtil {
 //        work.close();
         return list;
     }
+    /**
+     * 描述：获取IO流中的数据，组装成List<List<Object>>对象
+     *
+     * @param in,fileName
+     * @return
+     * @throws IOException
+     */
+    public List<List<Object>> getBankListByExcel4Statis(InputStream in, String fileName) throws Exception {
+        List<List<Object>> list = null;
 
+        //创建Excel工作薄
+        Workbook work = this.getWorkbook(in, fileName);
+        if (null == work) {
+            throw new Exception("创建Excel工作薄为空！");
+        }
+        Sheet sheet = null;
+        Row row = null;
+        Cell cell = null;
+
+        list = new ArrayList<List<Object>>();
+        //遍历Excel中所有的sheet
+        for (int i = 0; i < 1; i++) {
+            sheet = work.getSheetAt(i);
+            if (sheet == null) {
+                continue;
+            }
+
+            //遍历当前sheet中的所有行
+            for (int j = 0; j <= sheet.getLastRowNum(); j++) {
+                row = sheet.getRow(j);
+                //遍历所有的列
+                List<Object> li = new ArrayList<Object>();
+                for (int y = 0; y < row.getLastCellNum(); y++) {
+                    cell = row.getCell(y);
+                    li.add(cell == null ? "" : this.getCellValue4Statis(cell));
+                }
+                list.add(li);
+
+            }
+        }
+//        work.close();
+        return list;
+    }
     /**
      * 描述：根据文件后缀，自适应上传文件的版本
      *
@@ -214,6 +256,44 @@ public class ImportExcelUtil {
         return value;
     }
 
+    /**
+     * 描述：对表格中数值进行格式化
+     *
+     * @param cell
+     * @return
+     */
+    public Object getCellValue4Statis(Cell cell) {
+        Object value = null;
+        DecimalFormat df = new DecimalFormat("0");  //格式化number String字符
+        SimpleDateFormat sdf = new SimpleDateFormat("yyy-MM-dd HH:mm:ss");  //日期格式化
+        DecimalFormat df2 = new DecimalFormat("0.00");  //格式化数字
+
+        switch (cell.getCellType()) {
+            case Cell.CELL_TYPE_STRING:
+                value = cell.getRichStringCellValue().getString();
+                //去除前后空字符串
+                value = removeEmptyStrings(value.toString());
+                break;
+            case Cell.CELL_TYPE_NUMERIC:
+                if ("General".equals(cell.getCellStyle().getDataFormatString())) {
+                    value = df.format(cell.getNumericCellValue());
+                } else if ("m/d/yy".equals(cell.getCellStyle().getDataFormatString())) {
+                    value = sdf.format(cell.getDateCellValue());
+                } else {
+                    value = cell.getNumericCellValue();
+                }
+                break;
+            case Cell.CELL_TYPE_BOOLEAN:
+                value = cell.getBooleanCellValue();
+                break;
+            case Cell.CELL_TYPE_BLANK:
+                value = "";
+                break;
+            default:
+                break;
+        }
+        return value;
+    }
     /**
      * 去除数据前后空字符串
      * @param textContent
