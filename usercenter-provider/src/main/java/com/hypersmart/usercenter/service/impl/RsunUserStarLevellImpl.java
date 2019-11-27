e758a3e3ee62028fee2b813a7eae0b02be30e1bb
q569295515 <569295515@qq.com>
2019-11-27 11:07:36 +0800

    重新提交被覆盖的代码

--- usercenter-provider/src/main/java/com/hypersmart/usercenter/service/impl/RsunUserStarLevellImpl.java
+++ usercenter-provider/src/main/java/com/hypersmart/usercenter/service/impl/RsunUserStarLevellImpl.java
@@ -10,6 +10,7 @@ import com.hypersmart.framework.utils.StringUtils;
 import com.hypersmart.usercenter.controller.GridApprovalRecordController;
 import com.hypersmart.usercenter.mapper.RsunUserStarLevellMapper;
 import com.hypersmart.usercenter.mapper.UcUserMapper;
+import com.hypersmart.usercenter.model.RsunUserStarLevel;
 import com.hypersmart.usercenter.model.RsunUserStarLevell;
 import com.hypersmart.usercenter.model.UcUser;
 import com.hypersmart.usercenter.service.RsunUserStarlLevelService;
@@ -18,6 +19,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
 import java.util.*;
@@ -29,6 +31,36 @@ public class  RsunUserStarLevellImpl extends GenericService<String, RsunUserStar
 
     @Resource
     RsunUserStarLevellMapper rsunUserStarLevellMapper;
+
+    @Override
+    public PageList<RsunUserStarLevel> moneylist(QueryFilter queryFilter) {
+        if (null != queryFilter) {
+            PageBean pageBean = queryFilter.getPageBean();
+            if (!com.hypersmart.base.util.BeanUtils.isEmpty(pageBean)) {
+                PageHelper.startPage(pageBean.getPage(), pageBean.getPageSize(), pageBean.showTotal());
+            } else {
+                PageHelper.startPage(1, Integer.MAX_VALUE, false);
+            }
+            Map<String, Object> paramMap = queryFilter.getParams();
+            List<RsunUserStarLevel> list = rsunUserStarLevellMapper.getRsunUserStarLevelList(paramMap);
+            if (!CollectionUtils.isEmpty(list)) {
+                return new PageList(list);
+            } else {
+                PageList<RsunUserStarLevel> pageList = new PageList<>();
+                pageList.setRows(new ArrayList<>());
+                pageList.setTotal(0);
+                pageList.setPageSize(20);
+                pageList.setPage(1);
+                return pageList;
+            }
+        }
+        PageList<RsunUserStarLevel> pageList = new PageList<>();
+        pageList.setRows(new ArrayList<>());
+        pageList.setTotal(0);
+        pageList.setPageSize(20);
+        pageList.setPage(1);
+        return new PageList<>();
+    }
 
     //全年金币job
     public void doGoldJob4Gold(){
