d833f3f5f08ce0390195dbddabf4371ed930c9d2
liuhao <121935403@.qq.com>
2019-12-09 19:27:19 +0800

    第三封用户金币接口查询

--- usercenter-provider/src/main/java/com/hypersmart/usercenter/dto/GoldInfo.java
+++ usercenter-provider/src/main/java/com/hypersmart/usercenter/dto/GoldInfo.java
@@ -13,4 +13,59 @@ public class GoldInfo {
     private String project;
     private String sort;
 
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserAccount() {
+        return userAccount;
+    }
+
+    public void setUserAccount(String userAccount) {
+        this.userAccount = userAccount;
+    }
+
+    public String getGold() {
+        return gold;
+    }
+
+    public void setGold(String gold) {
+        this.gold = gold;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getProject() {
+        return project;
+    }
+
+    public void setProject(String project) {
+        this.project = project;
+    }
+
+    public String getSort() {
+        return sort;
+    }
+
+    public void setSort(String sort) {
+        this.sort = sort;
+    }
 }
